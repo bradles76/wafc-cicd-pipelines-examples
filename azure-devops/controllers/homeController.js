affd3f06bd9bf2fe114211cc539bb0734d5f97f7
Anthony Salemo <ajssalemo@gmail.com>
2023-02-02 17:16:42 -0500

    adding examples

--- azure-devops/controllers/homeController.js
+++ azure-devops/controllers/homeController.js
@@ -3,5 +3,5 @@ import * as express from "express";
 const router = express.Router()
 
 export const homeController = router.get("/", (_req, res) => {
-    res.json({ msg: "ephemeral-storage-node" })
+    res.json({ msg: "Hello, from Azure DevOps!" })
 })
--- azure-devops/server.js
+++ azure-devops/server.js
@@ -1,7 +1,5 @@
 import express from "express";
 import { homeController } from "./controllers/homeController.js";
-import { writeFileController } from "./controllers/writeFileController.js";
-import { readFileController } from "./controllers/readFileController.js";
 
 const app = express();
 const port = process.env.PORT || 3000;
