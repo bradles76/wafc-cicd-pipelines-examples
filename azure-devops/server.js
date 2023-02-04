61e64e84a950bc285d108530f7dee520c234cf70
Anthony Salemo <ajssalemo@gmail.com>
2023-02-03 22:26:32 -0500

    restructuring project

--- azure-devops/server.js
+++ azure-devops/server.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { homeController } from "./controllers/homeController.js";
+import { homeController } from "../controllers/homeController.js";
 
 const app = express();
 const port = process.env.PORT || 3000;
