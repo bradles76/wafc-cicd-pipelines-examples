2a080ec1cb27343b829d9c0693fa7891dc575c78
Anthony Salemo <ajssalemo@gmail.com>
2023-02-03 21:30:10 -0500

    testing path changes

---
+++ controllers/homeController.js
@@ -0,0 +1,7 @@
+import * as express from "express";
+
+const router = express.Router()
+
+export const homeController = router.get("/", (_req, res) => {
+    res.json({ msg: "Hello, from Azure DevOps!" })
+})
---
+++ server.js
@@ -0,0 +1,9 @@
+import express from "express";
+import { homeController } from "./controllers/homeController.js";
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use("/", homeController);
+
+app.listen(port, () => console.log(`Server is listening on port ${port}.`));
