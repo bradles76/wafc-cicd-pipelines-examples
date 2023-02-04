26b61ebf8400af19e416c61e8f86d5bec96ddcee
Anthony Salemo <ajssalemo@gmail.com>
2023-02-03 21:29:27 -0500

    testing paths

--- azure-devops/controllers/homeController.js
+++
@@ -1,7 +0,0 @@
-import * as express from "express";
-
-const router = express.Router()
-
-export const homeController = router.get("/", (_req, res) => {
-    res.json({ msg: "Hello, from Azure DevOps!" })
-})
