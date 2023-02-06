d64d8f2843b7e55b25f166e52a4a19d5f9f5caf9
Anthony Salemo <ajssalemo@gmail.com>
2023-02-05 23:28:27 -0500

    testing with sp credential login

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -3,5 +3,5 @@ import * as express from "express";
 const router = express.Router()
 
 export const homeController = router.get("/", (_req, res) => {
-    res.json({ msg: "Hello, from Azure DevOps!" })
+    res.json({ msg: "Hello, from GitHub Actions!" })
 })
